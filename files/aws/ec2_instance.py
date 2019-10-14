0cf994d6335a8be172a577dc46c2e2574d95b512
3r1co <3r1co@users.noreply.github.com>
2019-10-14 12:54:00 +0000

    Automated deployment: Mon Oct 14 12:54:00 UTC 2019 ee3965e74e17ef0791692ad0051405c729e165c2

--- files/aws/ec2_instance.py
+++ files/aws/ec2_instance.py
@@ -8,30 +8,15 @@ import troposphere.ec2 as ec2
 template = Template()
 
 template.add_mapping('RegionMap', {
-    "us-east-1": {"AMI": "ami-06e619a8d6b783fa3"},
-    "eu-west-1": {"AMI": "ami-089a2a65371000190"},
-    "ap-southeast-1": {"AMI": "ami-0e95b2e151e62f64c"}
+    "us-east-1": {"AMI": ""},
+    "eu-west-1": {"AMI": ""},
+    "ap-southeast-1": {"AMI": ""}
 })
 
 lc = template.add_resource(LaunchConfiguration(
     "Ec2Instance",
     ImageId=FindInMap("RegionMap", Ref("AWS::Region"), "AMI"),
-    InstanceType="t1.micro",
-    SecurityGroups=[ImportValue("prgtrn-SSHSecurityGroupID")],
-    IamInstanceProfile=ImportValue("prgtrn-EC2GlobalRole")
+    InstanceType="t1.micro"
 ))
-
-template.add_resource(AutoScalingGroup(  
-        "myASG",  
-        AutoScalingGroupName="asg-ibs-d-ew1-prgtrn-emuellen",   
-        DesiredCapacity="1", 
-        MinSize=1, 
-        MaxSize=1,
-        VPCZoneIdentifier=[ImportValue("prgtrn-PrivateSubnetAZa1Id"), ImportValue("prgtrn-PrivateSubnetAZb1Id"), ImportValue("prgtrn-PrivateSubnetAZc1Id")],  
-        LaunchConfigurationName=Ref(lc),   
-        Tags=[AutoScalingTag('gto:finops:projectcode', "R0S28973", True), AutoScalingTag('Name', "asg-ibs-d-ew1-prgtrn-emuellen", True)],
-        HealthCheckGracePeriod=300,
-        HealthCheckType="ELB"
-    )) 
 
 print(template.to_yaml())
