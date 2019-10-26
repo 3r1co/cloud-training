f6c9d1a861e0c1ad1d8f32b7af63eea78179a1e4
Eric Muellenbach <emuellenbach@gmx.de>
2019-10-26 14:23:27 +0200

    Update AMI IDs

--- docs/files/aws/ec2_instance.py
+++ docs/files/aws/ec2_instance.py
@@ -8,9 +8,8 @@ import troposphere.ec2 as ec2
 template = Template()
 
 template.add_mapping('RegionMap', {
-    "us-east-1": {"AMI": ""},
-    "eu-west-1": {"AMI": ""},
-    "ap-southeast-1": {"AMI": ""}
+    "us-east-1": {"AMI": "ami-00b882ac5193044e4"},
+    "eu-west-1": {"AMI": "ami-0f62aafc6efe8fd7b"}
 })
 
 lc = template.add_resource(LaunchConfiguration(
